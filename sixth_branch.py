d5d11742ae255f44756f3dcf259d24751a29e135
MarkevichAI <amarkevich@ecisys.com>
2021-03-22 07:49:39 +0300

    added fifth lesson

--- sixth_branch.py
+++ sixth_branch.py
@@ -33,6 +33,7 @@ class TrafficLight:
 
 TrafficLight("yellow").running()'''
 
+
 # 2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина). Значения данных атрибутов должны передаваться
 # при создании экземпляра класса. Атрибуты сделать защищенными. Определить метод расчета массы асфальта, необходимого для покрытия всего дорожного полотна.
 # Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв метра дороги асфальтом, толщиной в 1 см * чи сло см толщины полотна. Проверить работу метода.
@@ -45,11 +46,12 @@ class Road:
         self._length = length
         self._width = width
 
-    def mass_calculation(self, mass, thickness ):
+    def mass_calculation(self, mass, thickness):
         return self._width * self._length * thickness
 
 
 print(Road(3, 4).mass_calculation(5, 6))
+
 
 # 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность), income (доход). Последний атрибут должен быть защищенным
 # и ссылаться на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker. В классе
@@ -65,6 +67,7 @@ class Worker:
         self.position = position
         self._income = {"wage": wage, "bonus": bonus}
 
+
 class Position(Worker):
 
     def get_full_name(self):
@@ -77,7 +80,8 @@ class Position(Worker):
 pos = Position("asdasd", "yyyy", "werre", 23434, 3456)
 print(f"Name {pos.get_full_name()}, salary {pos.get_total_income()}")
 
-    # 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево). А также методы: go, stop, turn(direction), которые
+
+# 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево). А также методы: go, stop, turn(direction), которые
 # должны сообщать, что машина поехала, остановилась, повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый класс метод
 # show_speed, который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar переопределите метод show_speed. При значении скорости свыше 60 (TownCar) и
 # 40 (WorkCar) должно выводиться сообщение о превышении скорости.
@@ -85,6 +89,7 @@ print(f"Name {pos.get_full_name()}, salary {pos.get_total_income()}")
 
 
 class Car:
+    SPEED_LIMIT = 0
 
     def __init__(self, speed, color, name, is_police):
         self.speed = speed
@@ -103,6 +108,8 @@ class Car:
 
     def show_speed(self):
         print(f"Speed is: {self.speed}")
+        if (self.SPEED_LIMIT != 0) & (self.speed > self.SPEED_LIMIT):
+            print("Attention!! Speed is too high!!!\n")
 
 
 class SportCar(Car):
@@ -111,25 +118,17 @@ class SportCar(Car):
 
 
 class TownCar(Car):
+    SPEED_LIMIT = 60
 
     def __init__(self, speed, color, name):
         super(TownCar, self).__init__(speed, color, name, False)
 
-    def show_speed(self):
-        super(TownCar, self).show_speed()
-        if self.speed > 60:
-            print("Attention!! Speed is too high!!!\n")
-
 
 class WorkCar(Car):
+    SPEED_LIMIT = 40
 
     def __init__(self, speed, color, name):
         super(WorkCar, self).__init__(speed, color, name, False)
-
-    def show_speed(self):
-        super(WorkCar, self).show_speed()
-        if self.speed > 40:
-            print("Attention!! Speed is too high!!!\n")
 
 
 class PoliceCar(Car):
@@ -138,8 +137,9 @@ class PoliceCar(Car):
 
 
 TownCar(61, "asa", "kd").show_speed()
-WorkCar(61, "asa", "kd").show_speed()
+WorkCar(41, "asa", "kd").show_speed()
 SportCar(1161, "asa", "kd").show_speed()
+
 
 # 5. Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.”
 # Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен
